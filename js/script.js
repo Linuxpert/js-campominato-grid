931c59b9e6578650bf78864bcc4c8b357cb7d927
Linuxpert <g.manfre10@gmail.com>
2021-11-02 15:03:32 +0100

    primo approccio

---
+++ js/script.js
@@ -0,0 +1,4 @@
+//1.Chiedere all'utente il livello di difficoltà del gioco => promt
+//2.generare una griglia di gioco quadrata in base alla difficoltà => if
+    //2.1 contentente delle celle => ciclo for
+//3.quando l'utente clicca il quadrato cambia colore => click
