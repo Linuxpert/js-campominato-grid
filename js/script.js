3d25c08bce8a0f8de09d98eb20b0d36c4d367e1b
Linuxpert <g.manfre10@gmail.com>
2021-11-02 16:09:13 +0100

    prima parte di esercizio completata

--- js/script.js
+++ js/script.js
@@ -2,3 +2,75 @@
 //2.generare una griglia di gioco quadrata in base alla difficoltà => if
     //2.1 contentente delle celle => ciclo for
 //3.quando l'utente clicca il quadrato cambia colore => click
+
+const grid = document.getElementById("grid");
+
+//chiedere all'utente il livello di difficoltà da 1 a 3
+let difficultGame = parseInt(prompt("Inserisci un numero da 1 a 3 per la difficoltà, più il numero è alto più sale la difficoltà."));
+
+//controllo della difficoltà, se l'utente non inserisce un numero tra 1 e 3 il promt compare di nuovo
+while((difficultGame < 1) || (difficultGame > 3)) {
+    difficultGame = parseInt(prompt("Inserisci un numero da 1 a 3 per la difficoltà, più il numero è alto più sale la difficoltà."));
+}
+
+
+//in base alla difficoltà si generano tot quadrati
+if (difficultGame === 1) {
+
+    for(let i = 0; i < 100; i++){
+
+        let newElem = createSquare ("div", "square1");
+
+        newElem.addEventListener("click",
+            function(){
+                this.classList.add("clicked-true");
+            }
+        );
+
+        grid.appendChild(newElem);
+
+    }
+}else if(difficultGame === 2){
+
+    for(let i = 0; i < 81; i++){
+
+        let newElem = createSquare ("div", "square2");
+
+        newElem.addEventListener("click",
+            function(){
+                this.classList.add("clicked-true");
+            }
+        );
+
+        grid.appendChild(newElem);
+
+    }
+
+}else if(difficultGame === 3){
+
+    for(let i = 0; i < 49; i++){
+
+        let newElem = createSquare ("div", "square3");
+
+        newElem.addEventListener("click",
+            function(){
+                this.classList.add("clicked-true");
+            }
+        );
+
+        grid.appendChild(newElem);
+
+    }
+}
+
+
+//funzioni
+
+function createSquare (squareGen, classPlus) {
+    let nodeHundred = document.createElement(squareGen);
+    nodeHundred.classList.add(classPlus);
+
+    return nodeHundred;
+}
+
+
